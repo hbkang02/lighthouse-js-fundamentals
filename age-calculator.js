43d7c2a63b19cbe2fe7ecc699b3d01b1d96ec191
John Kang <hbkang02@gmail.com>
2022-09-29 20:58:30 -0700

    excercise calculate age

---
+++ age-calculator.js
@@ -0,0 +1,5 @@
+function ageCalculator (name, yearOfBirth, currentYear) {
+  const age = currentYear - yearOfBirth;
+  return name + ' is ' + age + ' years old.';
+}
+console.log(ageCalculator("Miranda", 1983, 2015));
